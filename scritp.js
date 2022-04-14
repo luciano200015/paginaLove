20aa4fc0a312d162d83d759533041a62714098b4
luciano200015 <102261002+luciano200015@users.noreply.github.com>
2022-04-14 12:36:31 -0400

    Add files via upload

---
+++ scritp.js
@@ -0,0 +1,9 @@
+
+function ocultar(param1,param2,param3,param4,param5){
+    document.getElementById(param1).style.display = "";
+    document.getElementById(param2).style.display = "none";
+    document.getElementById(param3).style.display = "none";
+    document.getElementById(param4).style.display = "none";
+    document.getElementById(param5).style.display = "none";
+
+}
